995f632120fa7620de35d3e37bb499b4cb0dc001
Devin Dixon <devindixon@Devins-MBP.lan>
2022-06-06 10:17:30 -0400

    Adding widgets

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Events from "./Events";
 import Config from "./Config";
 import Organizers from "./Organizers";
 import Templates from "./Templates";
+import Widgets from "./Widgets";
 
 class BWAPI {
     public static Auth: Auth = Auth;
@@ -14,6 +15,8 @@ class BWAPI {
     public static Organizers: Organizers = Organizers;
 
     public static Templates: Templates = Templates;
+
+    public static Widgets: Widgets = Widgets;
 }
 
 export {Auth};
