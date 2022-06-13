e17cbec1e79dae2e23054b15e13fd2699c06786f
Carina George <carinageorge@Devins-MacBook-Pro-2.local>
2022-06-13 11:11:49 -0400

    Working on VideosTS

--- src/Videos/Videos.ts
+++ src/Videos/Videos.ts
@@ -3,7 +3,61 @@ import Requests from "../Util/Requests";
 import RequestTypes from "../Util/RequestTypes";
 
 class Videos {
-    
+
+    private static routeCreateVideo : Route = {
+        route: "/videos",
+        method: RequestTypes.POST
+    }
+
+    public static routeUpdateVideo : Route = {
+        route: "/videos/{id}",
+        method: RequestTypes.PUT
+    }
+
+    private static routeListVideo : Route = {
+        route: "/videos",
+        method: RequestTypes.POST
+    } 
+
+    private static routeViewVideo: Route = { 
+        route : "/videos/{id}",
+        method : RequestTypes.GET
+    };
+
+    private static routeDeleteVideo: Route = { 
+        route : "/videos/{id}",
+        method : RequestTypes.DELETE
+    };
+
+    public static createVideo(data : object, query? : object | null, options? : object | null){
+
+        return Requests.post(this.routeCreateVideo.route, data, query, options);
+    }
+
+    public static updateVideo(video_id: string, data: object, query?: object | null, options?: object | null){
+        let route = this.routeUpdateVideo.route.replaceAll('{id}', video_id);
+	return Requests.put(route, data, query, options);
+    }
+
+    public static ListVideo(data : object, query? : object | null, options? : object | null){
+
+        return Requests.post(this.routeListVideo.route, data, query, options);
+    }
+
+    public static viewVideo(video_id : string, query? : object | null, options? : object | null) {
+        let route = this.routeViewVideo.route.replaceAll('{id}', video_id);
+        return Requests.get(route, query, options);
+    }
+
+    public static getVideo(query? : object | null, options? : object | null) {
+        return Requests.get(this.routeViewVideo.route, query, options);
+    }
+
+    public static deleteVideo(video_id : string, data : object, query? : object | null, options? : object | null) {
+        let route = this.routeDeleteVideo.route.replaceAll('{id}', video_id);
+        return Requests.delete(route, data, query, options);
+    }
+
 }
 
 export default Videos;
